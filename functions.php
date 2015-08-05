f57f1bae4fee8d589c10c50f56be56556dd015d9
Sprincis <sprincis.janis@oxior.eu>
2015-08-05 14:30:06 +0300

    v1.2 - Removed tb-testimonials.zip, using Easy Testimonials for testimonials

--- functions.php
+++ functions.php
@@ -922,7 +922,7 @@ function shopera_register_required_plugins() {
 			'slug'     				=> 'shopera-functionality', // The plugin slug (typically the folder name)
 			'source'   				=> get_template_directory() . '/inc/tgm-activation/plugins/shopera-functionality.zip', // The plugin source
 			'required' 				=> false, // If false, the plugin is only 'recommended' instead of required
-			'version' 				=> '1.0.0', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
+			'version' 				=> '1.1', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
 			'force_activation' 		=> false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch
 			'force_deactivation' 	=> true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
 			'external_url' 			=> '', // If set, overrides default API URL and points to an external URL
@@ -983,11 +983,10 @@ function shopera_register_required_plugins() {
 			'external_url' 			=> '', // If set, overrides default API URL and points to an external URL
 		),
 		array(
-			'name'     				=> 'TBTestimonials', // The plugin name
-			'slug'     				=> 'tb-testimonials', // The plugin slug (typically the folder name)
-			'source'   				=> get_template_directory() . '/inc/tgm-activation/plugins/tb-testimonials.zip', // The plugin source
+			'name'     				=> 'Easy Testimonials', // The plugin name
+			'slug'     				=> 'easy-testimonials', // The plugin slug (typically the folder name)
 			'required' 				=> false, // If false, the plugin is only 'recommended' instead of required
-			'version' 				=> '1.7.4', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
+			'version' 				=> '1.31.1', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
 			'force_activation' 		=> false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch
 			'force_deactivation' 	=> true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
 			'external_url' 			=> '', // If set, overrides default API URL and points to an external URL
